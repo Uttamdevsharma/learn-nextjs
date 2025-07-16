0c062993ef41573e221a65047dc701224386ae47
Uttam <uttamrohit4545@gmail.com>
2025-07-16 21:11:29 +0600

    feat : layout around fix route

---
+++ src/app/abc/layout.jsx
@@ -0,0 +1,13 @@
+
+export default function AbcLayout({children}) {
+    return (
+
+        <>
+        {children}
+        <ul>
+            <li>Dummy</li>
+            <li>Dummy</li>
+        </ul>
+        </>
+    )
+}
